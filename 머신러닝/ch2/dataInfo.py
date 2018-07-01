2a7acd4a2e2d7a3a8d635c83c3465a85d6587f2f
LeeSangHoon <dl57934@naver.com>
2018-07-01 23:35:47 +0900

    one-hotEncoding

--- 머신러닝/ch2/dataInfo.py
+++ 머신러닝/ch2/dataInfo.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
-from matplotlib import pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import Imputer, StandardScaler
+from sklearn.pipeline import Pipeline
 import numpy as np
+
 housing = pd.read_csv('./dataFile/housing.csv')
 
 housing["income_cat"] = np.ceil(housing["median_income"]/1.5)
@@ -12,11 +15,16 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
-#계층적 샘플링과 랜덤 샘플링을 합친 방법이다.
-housing = strat_train_set.copy()
-housing.plot(kind="scatter", x="longitude", y="latitude",
-             alpha=0.4, s=housing["population"]/100, label="population", figsize=(10, 7),
-             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True, sharex=False
-             )
-plt.show()
+encoder = OneHotEncoder()
+
+housing_cat = housing["ocean_proximity"]
+housing_cat_encoded, housing_categories = housing_cat.factorize()
+housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1))
+onHot = housing_cat_1hot.toarray()
+for i in onHot:
+    print(i)
+#계층적 샘플링 + 랜덤 샘플링
+
+
+
 
