a1c14868bad83153f2d84ab1c027e440fad4c58c
unknown <dl57934@naver.com>
2018-09-27 19:20:45 +0900

    add Dnn Classifier

---
+++ 머신러닝/ch10/2.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.astype(np.float32).reshape(-1, 28*28) / 255.0
+X_test = X_test.astype(np.float32).reshape(-1, 28*28) / 255.0
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+feature_cols = [tf.feature_column.numeric_column("X", shape=[28 * 28])]
+dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300, 100], n_classes=10,
+                                     feature_columns=feature_cols)
+
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"X": X_train}, y=y_train, num_epochs=40, batch_size=50, shuffle=True)
+dnn_clf.train(input_fn=input_fn)
+y_pred = dnn_clf.predict(X_test)
+print(y_pred)
+
