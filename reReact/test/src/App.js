4dcb033f316a54d1228a78da7873e34033481324
dl57934 <dl57934@pukyong.ac.kr>
2018-11-19 11:49:04 +0900

    how to make react Component

---
+++ reReact/test/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FunctionComponent from './FunctionComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FunctionComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ reReact/test/src/FunctionComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FunctionComponent = ({props})=>{
+    return ("함수형 컴포넌트야")
+}
+
+export default FunctionComponent;
