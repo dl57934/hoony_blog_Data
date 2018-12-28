3ccac0903ad0c0c252ed32a2ef2fdc0c69f08dee
dl57934 <dl57934@pukyong.ac.kr>
2018-12-28 18:54:40 +0900

    making normal Singleton by Java

---
+++ designPattern/Singleton/java/Basic/Main.java
@@ -0,0 +1,16 @@
+
+
+class Main{
+	public static void main(String[] args){
+		Singleton obj1 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance();
+		
+		if(obj1.equals(obj2)){
+			System.out.println("동일하다");
+		}
+		else {
+			System.out.println("다르다");
+		}
+		System.out.println("END");
+	}
+}
