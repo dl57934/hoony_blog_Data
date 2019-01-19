2ec6bb299a1a00774a47da8f65f0e0c7eb15e329
dl57934 <dl57934@pukyong.ac.kr>
2019-01-19 22:12:11 +0900

    first Time Composite

---
+++ designPattern/Entry/Basic/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args){
+        try{
+            Directory rootdir = makeDirectory("root");
+            Directory bindir = makeDirectory("bin");
+            Directory tmpdir = makeDirectory("tmp");
+            Directory usrdir = makeDirectory("usr");
+            rootdir.add(bindir);
+            rootdir.add(tmpdir);
+            rootdir.add(usrdir);
+            bindir.add(makeFile("vi", 10000));
+            bindir.add(new File("latex", 20000));
+            rootdir.printList();
+
+        }catch (FileTreatmentException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Directory makeDirectory(String name){
+        return new Directory(name);
+    }
+    public static File makeFile(String name, int size){
+        return new File(name, size);
+    }
+}
