9971cd7f1aa2aaa09e91c7e92e6fe8641566b735
dl57934 <dl57934@pukyong.ac.kr>
2019-01-24 23:39:47 +0900

    chain of Responsibility

--- designPattern/ChainOfResponsibility/Basic/OddSupport.java
+++ designPattern/ChainOfResponsibility/Basic/OddSupport.java
@@ -2,7 +2,11 @@ public class OddSupport extends Support{
 	public OddSupport(String name){
 		super(name);
 	}
-	protected boolean resolve(){
-		
+	protected boolean resolve(Trouble trouble){
+		if(trouble.getNumber() % 2 == 1){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
