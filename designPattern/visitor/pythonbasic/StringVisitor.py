fc6d599358989580882bb2831f7c4541994d41ac
dl57934 <dl57934@pukyong.ac.kr>
2019-01-30 02:51:30 +0900

    implement visitor by python

--- designPattern/visitor/pythonbasic/StringVisitor.py
+++ designPattern/visitor/pythonbasic/StringVisitor.py
@@ -1,5 +1,6 @@
 from Visitor import Visitor
 
+
 class StringVisitor(Visitor):
 
     def __init__(self):
@@ -7,13 +8,12 @@ class StringVisitor(Visitor):
         self.save = ""
 
     def visitFile(self, file):
-        print(self.prefix+"/"+file.getName())
+        print(self.prefix + "/" + file.getName()+" ("+str(file.getSize())+")")
 
     def visitDirectory(self, directory):
-        print(self.prefix+"/"+directory.getName())
+        print(self.prefix + "/" + directory.getName()+" ("+str(directory.getSize())+")")
         self.save = self.prefix
-        self.prefix = self.prefix+"/"+directory.getName()
+        self.prefix = self.prefix + "/" + directory.getName()
         for file_or_dir in directory.documents:
             file_or_dir.accept(self)
         self.prefix = self.save
-
