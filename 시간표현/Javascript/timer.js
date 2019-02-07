ae4480f38748fe28f52ac402cfe550a98b7f5e15
dl57934 <dl57934@pukyong.ac.kr>
2019-02-07 15:59:28 +0900

    add timer Exporession

---
+++ 시간표현/Javascript/timer.js
@@ -0,0 +1,10 @@
+var now = new Date();
+
+console.log("현재 년"+now.getFullYear());
+console.log("현재 월",now.getMonth()+1);
+console.log("현재 일"+ now.getDate());
+console.log("현재 시간"+ now.getHours());
+console.log("현재 분"+ now.getMinutes());
+console.log("현재 초"+ now.getSeconds());
+console.log("현재 요일"+ now.getDay());
+// 0(일요일) ~ 6(토요일) 
