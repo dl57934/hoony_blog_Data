2f11cba96e2c829be10a0337f1c89ddb5e7c3673
이상훈 <dl57934@naver.com>
2020-02-29 01:19:20 +0900

    destructuring

--- typescript/Variable/const.ts
+++ typescript/Variable/const.ts
@@ -1,1 +1,15 @@
 const numLivesForCat = 9;
+
+let input = [1, 2];
+let [first, second] = input;
+console.log(first); // 1;
+console.log(second); // 2;
+
+function f([first, second]: [number, number]) {
+  console.log(first);
+  console.log(second);
+}
+
+f([1, 2]);
+
+const [third, ...forth] = [1, 2, 3, 4];
