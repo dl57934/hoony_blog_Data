40a1983ca56d50dc8db792294dc181bacf25441c
이상훈 <dl57934@naver.com>
2020-03-30 17:36:02 +0900

    review Go

--- golang/start.go
+++ golang/start.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	fmt.Println(t.S)
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	var x = [4]int{1, 2, 3, 4}
+	t := x[:]
+	fmt.Println("%v", t)
+}
+func describe(i *T) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
